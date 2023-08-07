6b147e0b9dbffbda3629867a8a1fe071341d001f
VishalP-SoluteLabs <pansuriyavishal1308@gmail.com>
2023-08-07 11:21:51 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -4,37 +4,53 @@ import './App.css';
 function App() {
 
   const [num, setNum] = useState(0)
+  const [name, setName] = useState('')
 
   const handleClick = () => {
     setNum(num + 1)
   }
 
+  
   //useEffect-01
+  useEffect(() => {
+    console.log(`Running useEffect without any dependency:- Count ${num}`)
+  })
+
+
+  //useEffect-02
   useEffect(() => {
     console.log('Using useEffect!!')
   }, [])
   
 
-  //useEffect-02
+  //useEffect-03
   useEffect(() => {
     console.log(`Component did updated: ${num}`)
   }, [num])
   
 
-  //useEffect-03
+  //useEffect-04
+  useEffect(() => {
+    console.log(`Name: ${name} or Num: ${num} has changed`)
+  },[name, num])
+  
+
+  //useEffect-05
   useEffect(() => {
     console.log('Component did mount')
     
     return () => {
       console.log("component will unmount")
     }
-  },[])
+  }, [])
+
 
   return (
     <div className='App'>
       <div className='App-header'>
         <div>
           <h3>{num}</h3>
+          <input value={name} onChange={e => setName(e.target.value)}/>
           <button className='btn' onClick={handleClick}>Add</button>
         </div>
       </div>
